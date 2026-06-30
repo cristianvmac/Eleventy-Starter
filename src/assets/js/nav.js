d686ffc286624b512fabb14e4ec4f0b06860f764
Cristian Marinescu <cristian.marinescu92@yahoo.com>
2026-06-30 21:01:08 +0200

    minor change in index for sbs-r

--- src/assets/js/nav.js
+++ src/assets/js/nav.js
@@ -250,15 +250,12 @@
 	init.eventListeners();
 })();
 
-
-
-
     // faq
     const faqItems = Array.from(document.querySelectorAll('.cs-faq-item'));
         for (const item of faqItems) {
             const onClick = () => {
             item.classList.toggle('active')
         }
-        item.addEventListener('click', onClick)
+        	item.addEventListener('click', onClick)
         }
 
